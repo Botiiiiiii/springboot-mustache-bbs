aa2f8ad1dffcef0700277592a12b7d9dde6f7861
jinwoooooo <song427j@gmail.com>
2022-11-07 13:59:41 +0900

    footer 적용

--- src/main/java/com/mustache/bbs/controller/MustacheController.java
+++ src/main/java/com/mustache/bbs/controller/MustacheController.java
@@ -7,10 +7,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MustacheController {
-
+    @GetMapping(value = "/hi")
+    public String mustacheCon(Model model) {
+        model.addAttribute("username", "song");
+        return "greetings";
+    }
     @GetMapping(value = "/hi/{id}")
     public String mustacheCon2(@PathVariable String id, Model model) {
-        model.addAttribute("username", "rok");// view에 값을 넘기겠
+        model.addAttribute("username", "song");// view에 값을 넘기겠
         model.addAttribute("id", id);// view에 값을 넘기겠
         return "greetings"; // greetings라는 이름의의 view를 리턴
     }
