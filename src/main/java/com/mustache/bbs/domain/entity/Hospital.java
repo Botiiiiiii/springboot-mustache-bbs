a3aa7deebc99d56b140521d7efc4c9f7f933bcf0
jinwoooooo <song427j@gmail.com>
2022-11-11 13:45:49 +0900

    Hospital DB 추가

---
+++ src/main/java/com/mustache/bbs/domain/entity/Hospital.java
@@ -0,0 +1,37 @@
+package com.mustache.bbs.domain.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "nation_wide_hospitals")
+@NoArgsConstructor
+@Getter
+public class Hospital {
+    @Id
+    private Integer id;
+//    private String openServiceName;
+//    private Integer openLocalGovernmentCode;
+//    private String managementNumber;
+//    private LocalDateTime licenseDate;
+//    private Integer businessStatus;
+//    private Integer businessStatusCode;
+//    private String phone;
+//    private String fullAddress;
+//    private String roadNameAddress;
+    @Column(name = "hospital_name")
+    private String hospitalName;
+//    private String businessTypeName;
+//    private Integer healthcareProviderCount;
+//    private Integer patientRoomCount;
+//    private Integer totalNumberOfBeds;
+//    private float totalAreaSize;
+
+}
